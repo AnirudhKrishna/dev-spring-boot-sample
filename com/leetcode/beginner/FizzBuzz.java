804bbca539940a97c957e90fa326d3ce32b16457
AnirudhKrishna <anirudhkrishna26@gmail.com>
2024-03-17 11:31:40 +0530

    Day-03 - fizz-buzz -code refactor

--- com/leetcode/beginner/FizzBuzz.java
+++ com/leetcode/beginner/FizzBuzz.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class FizzBuzz {
     public static void main(String[] args) {
-        int n = 30;
+        int n = 31;
         List<String> strings = new ArrayList<>();
 
         for (int i = 1; i <= n; i++) {
